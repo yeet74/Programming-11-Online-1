04946181f3d5897eb8138d49813a28ceab960faf
Paul Zaremba <pzaremba@vsb.bc.ca>
2018-07-16 15:53:02 -0700

    Uploading

---
+++ Expressions Assignment/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String args[]){
+        /*For the following expressions write out using code step by step how the
+        compiler will evaluate it
+        Example:
+        System.out.println(2 * 5 + 3);
+        System.out.println(10 + 3);
+        System.out.println(13);
+        */
+        //Expression 1
+
+
+        //Expression 2
+
+
+        //Expression 3
+        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+
+        //Expression 4
+
+
+        //Expression 5
+
+    }
+}
