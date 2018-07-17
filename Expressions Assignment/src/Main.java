c77df309e82ffc532128f899ea77ff49a33511ab
Paul Zaremba <pzaremba@vsb.bc.ca>
2018-07-17 13:21:02 -0700

    Expressions Finished

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -6,20 +6,26 @@ public class Main {
         System.out.println(2 * 5 + 3);
         System.out.println(10 + 3);
         System.out.println(13);
+        Only complete one computation per line of code
+        The output for every line is always equal to the same number
+        In example above the out put should be
+        13
+        13
+        13
         */
         //Expression 1
         System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
 
         //Expression 2
-
+        System.out.println(23%4 + (18 - 3 / 2));
 
         //Expression 3
         System.out.println(((8 - 5) + Math.pow(3,4)/5));
 
         //Expression 4
-
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
 
         //Expression 5
-
+        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
     }
 }
