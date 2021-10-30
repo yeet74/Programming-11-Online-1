d97b19af091c20fda0df847a450ed268920dfb87
yeet74 <59450943+yeet74@users.noreply.github.com>
2021-10-29 20:26:29 -0700

    Update Main.java

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -13,19 +13,47 @@ public class Main {
         13
         13
         */
-        //Expression 1
+        //Expression 
         System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
-
-        //Expression 2
+        System.out.println(5+ 10/2 - (4 - 8)/3);
+        System.out.println((5+ 5- (4 - 8)/3);
+        System.out.println(5+ 5 - (-4)/3);
+        System.out.println(10- (-4)/3);
+        System.out.println(10 - (-1);
+        System.out.println(11);
+        //Expression 
         System.out.println(23%4 + (18 - 3 / 2));
-
-        //Expression 3
+        System.out.println(3 + (18 - 3/2));
+        System.out.println(3 + (18 -1));
+        System.out.println(3 + 17);
+        System.out.println(20);
+        //Expression 
         System.out.println(((8 - 5) + Math.pow(3,4)/5));
-
+        System.out.println((3 + Math.pow(3,4)/5));
+        System.out.println((3 + 81/5));
+        System.out.println((3 + 16));
+        System.out.println(19);
         //Expression 4
         System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + 8 / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + 8 / 8 + (9 / 3));
+        System.out.println(1 - 8 + 1 + (9 / 3));
+        System.out.println(1 - 8 + 1 + 3);
+        System.out.println(-7 + 1 + 3);
+        System.out.println(-6 + 3);
+        System.out.println(-3);
 
         //Expression 5
         System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println(((7 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println((10 * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println((10 * 0 + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println((10 * 0 + 2) - Math.pow(5, 3));
+        System.out.println((10 * 0 + 2) - 125);
+        System.out.println((0 + 2) - 125);
+        System.out.println( 2 - 125);
+        System.out.println(-123);
+
     }
 }
